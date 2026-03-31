98281d404383396b518f8edb73b696d0e3e3cf7f
isynreal <isynreal@gmail.com>
2026-03-31 15:23:37 +0800

    0331v1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/video-player/', // <--- 請注意：這裡要改成 '/您的儲存庫名稱/' (前後都要有斜線)
+  base: '/', // <--- 請注意：這裡要改成 '/您的儲存庫名稱/' (前後都要有斜線)
 })
